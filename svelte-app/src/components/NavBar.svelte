6740a24f33759a138e884c1910ece9e99a316ddc
yelim421 <jin-yl18@mails.tsinghua.edu.cn>
2021-08-04 20:10:06 +0900

    add authentication

--- svelte-app/src/components/NavBar.svelte
+++ svelte-app/src/components/NavBar.svelte
@@ -1,11 +1,26 @@
 <script>
-  import { Router, Link, Route } from 'svelte-routing';
-  import { getContext } from 'svelte';
-  import { ROUTER } from 'svelte-routing/src/contexts';
+  import { onMount } from 'svelte';
+  import { Link } from 'svelte-routing';
 
-  const { activeRoute } = getContext(ROUTER);
+  const providers = ['twitter', 'github', 'aad'];
+  const redirect = window.location.pathname;
+  let userInfo = undefined;
 
-  function getProps({ location, href, isPartiallyCurrent, isCurrent }) {
+  onMount(async () => (userInfo = await getUserInfo()));
+
+  async function getUserInfo() {
+    try {
+      const response = await fetch('/.auth/me');
+      const payload = await response.json();
+      const { clientPrincipal } = payload;
+      return clientPrincipal;
+    } catch (error) {
+      console.error('No profile could be found');
+      return undefined;
+    }
+  }
+
+  function getProps({ href, isPartiallyCurrent, isCurrent }) {
     const isActive = href === '/' ? isCurrent : isPartiallyCurrent || isCurrent;
 
     // The object returned here is spread on the anchor element's attributes
@@ -16,10 +31,36 @@
   }
 </script>
 
-<nav class="column is-2 menu">
-  <p class="menu-label">Menu</p>
-  <ul class="menu-list">
-    <Link to="/products" {getProps}>Products</Link>
-    <Link to="/about" {getProps}>About</Link>
-  </ul>
-</nav>
+<div class="column is-2">
+  <nav class="menu">
+    <p class="menu-label">Menu</p>
+    <ul class="menu-list">
+      <Link to="/products" {getProps}>Products</Link>
+      <Link to="/about" {getProps}>About</Link>
+    </ul>
+  </nav>
+  <nav class="menu auth">
+    <p class="menu-label">Auth</p>
+    <div class="menu-list auth">
+      {#if !userInfo}
+        {#each providers as provider (provider)}
+          <a href={`/.auth/login/${provider}?post_login_redirect_uri=${redirect}`}>
+            {provider}
+          </a>
+        {/each}
+      {/if}
+      {#if userInfo}
+        <a href={`/.auth/logout?post_logout_redirect_uri=${redirect}`}>
+          Logout
+        </a>
+      {/if}
+    </div>
+  </nav>
+  {#if userInfo}
+    <div class="user">
+      <p>Welcome</p>
+      <p>{userInfo && userInfo.userDetails}</p>
+      <p>{userInfo && userInfo.identityProvider}</p>
+    </div>
+  {/if}
+</div>
